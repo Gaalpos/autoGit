1f167370b441dbb41273f68a62a866221acbe2d8
Gaalpos <gabripazos13@gmail.com>
2023-01-19 17:06:59 +0100

    01/19/2023 17:06:58

---
+++ AutoPull.ps1
@@ -0,0 +1,14 @@
+function Fpull($route) {
+    Set-Location $route
+    Get-ChildItem
+    git pull
+
+}
+
+
+Fpull("D:\DAM\autoPull")
+Fpull("D:\DAM\bbdd")
+Fpull("D:\DAM\entornos")
+Fpull("D:\DAM\marcas")
+Fpull("D:\DAM\programacion")
+Set-Location ..
