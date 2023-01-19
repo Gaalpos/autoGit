9c9e553f4354a2d98a2405c3e9ab84c8bd774302
Gaalpos <gabripazos13@gmail.com>
2023-01-19 16:43:37 +0100

    01/19/2023 16:43:37

--- AutoPushWithDate.ps1
+++ AutoPushWithDate.ps1
@@ -1,14 +1,26 @@
-# function Fpull($route) {
-#     Set-Location $route
-#     Get-ChildItem
-#     git add .
-#     git commit -m 
-# }
+
+function Fpull($route) {
+    $date = Get-Date
+    $coma = '"'
+    $fullDate = $coma + $date + $coma
+    Write-Output $fullDate
+    Set-Location $route
+    Get-ChildItem
+    git add .
+    git commit -m $fullDate
+    git push origin master
+
+}
 
 
-# Fpull("D:\DAM\bbdd")
-# Fpull("D:\DAM\entornos")
-# Fpull("D:\DAM\marcas")
-# Fpull("D:\DAM\programacion")
-git add .
-git commit -m Get-Date
+Fpull("D:\DAM\autoPull")
+Fpull("D:\DAM\bbdd")
+Fpull("D:\DAM\entornos")
+Fpull("D:\DAM\marcas")
+Fpull("D:\DAM\programacion")
+Set-Location ..
+
+
+
+
+
